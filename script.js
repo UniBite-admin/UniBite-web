842a0d02106a3a7168b6510032b29a2da7d5c717
UniBite <admin@unibite.app>
2025-08-24 01:46:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
 
-            // Calculate offset based on fixed header and nav height
             const headerHeight = document.querySelector('header').offsetHeight;
             const navHeight = document.querySelector('nav').offsetHeight;
             const offset = headerHeight + navHeight;
 
-            // Calculate scroll position to center section header below fixed headers
             const scrollPosition = targetElement.offsetTop - offset;
 
-            // Scroll to the target element with offset
             window.scrollTo({
                 top: scrollPosition,
                 behavior: 'smooth'
