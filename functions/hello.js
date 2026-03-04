4c4e0d06fcbae07ce0219ced5a20f30030326ff9
UniBite <admin@unibite.app>
2026-03-04 01:29:58 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,8 @@
 export function onRequest() {
-  return new Response("hello from pages functions");
+  return new Response("HELLO FUNCTION HIT", {
+    headers: {
+      "Content-Type": "text/plain",
+      "X-PopBite-Function": "hello"
+    }
+  });
 }
