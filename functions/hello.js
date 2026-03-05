72ad9c47f58ab85a7168bcd58f3fffd4eb74bf08
UniBite <admin@unibite.app>
2026-03-06 00:21:41 +0100

    Delete functions/hello.js

--- functions/hello.js
+++
@@ -1,8 +0,0 @@
-export function onRequest() {
-  return new Response("HELLO FUNCTION HIT", {
-    headers: {
-      "Content-Type": "text/plain",
-      "X-PopBite-Function": "hello"
-    }
-  });
-}
